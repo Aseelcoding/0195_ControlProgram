82e6ad0ce6dfde09398de30fd49db8662f7842c9
Aseel <asilalhashmi@email.com>
2026-03-31 11:55:20 +0700

    add procedure to print the battery status using else if

--- 0195_ControlProgram.cpp
+++ 0195_ControlProgram.cpp
@@ -3,12 +3,37 @@
 #include <iostream>
 using namespace std;
 
+//Simple If Statement
+//check if the age upper than 18
 bool IsAgeUpper18(short Age) 
 {
     if (Age < 18)
         return false;
     else return true;
 }
+
+//else if
+//Battery level indicator
+void BatteryIndicator(short BatterLevel) 
+{
+    if (BatterLevel==100)
+    {
+        cout << "Full Charge Unplug charger\n";
+    }
+    else if (BatterLevel>=20)
+    {
+        cout << "Battery ok\n";
+    }
+    else if (BatterLevel>=10&& BatterLevel<20)
+    {
+        cout << "Low Battery\n";
+    }
+    else if (BatterLevel<10)
+    {
+        cout << "Critical! Phone shutting down";
+    }
+}
+
 int main()
 {
   
